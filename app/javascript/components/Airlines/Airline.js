0218f151bc2268df3d67cc1b5da14a673cd3810d
Naoki Hirahara <genenao21@gmail.com>
2021-01-20 13:14:14 +0900

    Add fetchData axios

--- app/javascript/components/Airlines/Airline.js
+++ app/javascript/components/Airlines/Airline.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
-const Airline = () => {
+const Airline = (props) => {
+    const { name, image_url, slug } = props.attributes
+
     return (
-        <div>
-            This is Airlines#show
+        <div className="card">
+            こんにちは
+            <div className="airline-logo">
+                <img src={image_url} alt={name} width="50" />
+            </div>
+            <div className="airline-name">
+                {name}
+            </div>
+            <div className="link-wrapper">
+                <Link to={"/" + slug}>View Airline</Link>
+            </div>
         </div>
     );
 };
--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Airline from "./Airline";
 
 const Airlines = () => {
+    const [airlines, setAirlines] = useState([])
+
+     useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios.get('/api/v1/airlines.json')
+            console.log(result.data.data)
+            setAirlines(result.data.data)
+        }
+        fetchData()
+    }, [])
+
+     const grid = airlines.map( (airline, index) => {
+         return (<Airline key={index} attributes={airline.attributes} />)
+     })
+
     return (
-        <div>
-            This is Airlines#index
+        <div className='home'>
+            <div className="header">
+<h1>header</h1>
+            </div>
+
+            <div className="grid">
+                {grid}
+            </div>
         </div>
     );
 };
