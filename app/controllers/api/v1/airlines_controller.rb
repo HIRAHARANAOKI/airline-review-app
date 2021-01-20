2aab4cde3445ba21f0eaf535913daf9db39aa5bd
Naoki Hirahara <genenao21@gmail.com>
2021-01-20 11:43:34 +0900

    Add railsAPI

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,16 +1,19 @@
 module Api
   module V1
     class AirlinesController < ApplicationController
+      # create時の422エラーを解決
+      protect_from_forgery with: :null_session
+
       def index
         airlines = Airline.all
 
-        render json: AirlineSerializer.new(airlines).serialized_json
+        render json: AirlineSerializer.new(airlines, options).serialized_json
       end
 
       def show
         airline = Airline.find_by(slug: params[:slug])
 
-        render json: AirlineSerializer.new(airlines).serialized_json
+        render json: AirlineSerializer.new(airline, options).serialized_json
       end
 
       def create
@@ -19,13 +22,38 @@ module Api
         if airline.save
           render json:AirlineSerializer.new(airline).serialized_json
         else
-          render json: {error: airline.errors.messages},status: 422
+          render json: {error: airline.errors.messages},status:422
         end
       end
+
+      def update
+        airline = Airline.find_by(slug: params[:slug])
+
+        if airline.update(airline_params)
+          render json:AirlineSerializer.new(airline, options).serialized_json
+        else
+          render json: {error: airline.errors.messages }, status:422
+        end
+      end
+
+      def destroy
+        airline = Airline.find_by(slug: params[:slug])
+
+        if airline.destroy
+          head :no_content
+        else
+          render json: {errors: airline.errors.message}, status: 422
+        end
+      end
+
       private
 
       def airline_params
         params.require(:airline).permit(:name, :image_url)
+      end
+
+      def options
+        @option ||= { include: %i[reviews] }
       end
 
     end
